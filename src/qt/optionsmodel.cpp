2f4c0a7c0a9f42bb5633d8ca1ae973cae441d0ef
Ryan Ofsky <ryan@ofsky.org>
2019-04-29 15:29:00 -0400

    Migrate -lang setting from QSettings to settings.json

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -138,6 +138,7 @@ void OptionsModel::Init(bool resetSettings)
     ProxySetting onion = ParseProxyString(ToQString(node().getPersistentSetting("onion")));
     m_onion_ip = onion.ip;
     m_onion_port = onion.port;
+    language = ToQString(node().getPersistentSetting("lang"));
 
     checkAndMigrate();
 
@@ -194,13 +195,9 @@ void OptionsModel::Init(bool resetSettings)
     if (node().isSettingIgnored("server")) addOverriddenOption("-server");
     if (node().isSettingIgnored("proxy")) addOverriddenOption("-proxy");
     if (node().isSettingIgnored("onion")) addOverriddenOption("-onion");
+    if (node().isSettingIgnored("lang")) addOverriddenOption("-lang");
 
     // If setting doesn't exist create it with defaults.
-    //
-    // If gArgs.SoftSetArg() or gArgs.SoftSetBoolArg() return false we were overridden
-    // by command-line and show this in the UI.
-
-    // Main
     if (!settings.contains("strDataDir"))
         settings.setValue("strDataDir", GUIUtil::getDefaultDataDirectory());
 
@@ -211,14 +208,6 @@ void OptionsModel::Init(bool resetSettings)
     }
     m_sub_fee_from_amount = settings.value("SubFeeFromAmount", false).toBool();
 #endif
-
-    // Display
-    if (!settings.contains("language"))
-        settings.setValue("language", "");
-    if (!gArgs.SoftSetArg("-lang", settings.value("language").toString().toStdString()))
-        addOverriddenOption("-lang");
-
-    language = settings.value("language").toString();
 
     if (!settings.contains("UseEmbeddedMonospacedFont")) {
         settings.setValue("UseEmbeddedMonospacedFont", "true");
@@ -403,7 +392,7 @@ QVariant OptionsModel::getOption(OptionID option) const
     case ThirdPartyTxUrls:
         return strThirdPartyTxUrls;
     case Language:
-        return settings.value("language");
+        return ToQVariant(node().getPersistentSetting("lang"), "");
     case UseEmbeddedMonospacedFont:
         return m_use_embedded_monospaced_font;
     case CoinControlFeatures:
@@ -545,8 +534,8 @@ bool OptionsModel::setOption(OptionID option, const QVariant& value)
         }
         break;
     case Language:
-        if (settings.value("language") != value) {
-            settings.setValue("language", value);
+        if (changed()) {
+            node().updateSetting("lang", ToSetting(value, QVariant::String));
             setRestartRequired(true);
         }
         break;
@@ -700,6 +689,7 @@ void OptionsModel::checkAndMigrate()
     migrate_setting(ProxyUse, "fUseProxy", "proxy");
     migrate_setting(ProxyIPTor, "addrSeparateProxyTor", "onion");
     migrate_setting(ProxyUseTor, "fUseSeparateProxyTor", "onion");
+    migrate_setting(Language, "language", "lang");
 
     // In case migrating QSettings caused any settings value to change, rerun
     // parameter interaction code to update other settings. This is particularly
