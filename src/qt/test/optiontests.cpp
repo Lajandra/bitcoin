e35b4f0ce891b53d20caa09a5222abbaf5953703
Ryan Ofsky <ryan@ofsky.org>
2019-04-29 15:29:00 -0400

    Unify bitcoin-qt and bitcoind persistent settings
    If a bitcoind setting like pruning, port mapping, or a network proxy is enabled
    in the GUI, it will now be stored in the bitcoin persistent setting file and
    shared with bitcoind, instead of being stored as Qt settings backed by the
    windows registry or platform specific config files.

--- src/qt/test/optiontests.cpp
+++ src/qt/test/optiontests.cpp
@@ -12,6 +12,8 @@
 
 #include <univalue.h>
 
+#include <fstream>
+
 //! Entry point for BitcoinApplication tests.
 void OptionTests::optionTests()
 {
@@ -28,4 +30,44 @@ void OptionTests::optionTests()
         settings.rw_settings.erase("prune");
     });
     gArgs.WriteSettingsFile();
+
+    BasicTestingSetup test{CBaseChainParams::REGTEST};
+    m_node.setContext(&test.m_node);
+    m_node.updateSetting("wallet", {}); // Clear setting to be independent of ENABLE_WALLET
+
+    QSettings settings;
+    settings.setValue("nDatabaseCache", 600);
+    settings.setValue("nThreadsScriptVerif", 12);
+    settings.setValue("fUseUPnP", false);
+    settings.setValue("fListen", false);
+    settings.setValue("bPrune", true);
+    settings.setValue("nPruneSize", 3);
+    settings.setValue("fUseProxy", true);
+    settings.setValue("addrProxy", "proxy:123");
+    settings.setValue("fUseSeparateProxyTor", true);
+    settings.setValue("addrSeparateProxyTor", "onion:234");
+
+    settings.sync();
+
+    OptionsModel options(m_node);
+    QVERIFY(!settings.contains("nDatabaseCache"));
+    QVERIFY(!settings.contains("nThreadsScriptVerif"));
+    QVERIFY(!settings.contains("fUseUPnP"));
+    QVERIFY(!settings.contains("fListen"));
+    QVERIFY(!settings.contains("bPrune"));
+    QVERIFY(!settings.contains("nPruneSize"));
+    QVERIFY(!settings.contains("fUseProxy"));
+    QVERIFY(!settings.contains("addrProxy"));
+    QVERIFY(!settings.contains("fUseSeparateProxyTor"));
+    QVERIFY(!settings.contains("addrSeparateProxyTor"));
+
+    std::ifstream file(gArgs.GetDataDirNet() / "settings.json");
+    QCOMPARE(std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()).c_str(), "{\n"
+        "    \"dbcache\": 600,\n"
+        "    \"listen\": false,\n"
+        "    \"onion\": \"onion:234\",\n"
+        "    \"par\": 12,\n"
+        "    \"proxy\": \"proxy:123\",\n"
+        "    \"prune\": 2861\n"
+        "}\n");
 }
