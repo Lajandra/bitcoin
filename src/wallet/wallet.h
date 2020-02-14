5a9581a270a42449248c1b9a19ea619ca77866bf
Russell Yanofsky <russ@yanofsky.org>
2020-02-14 12:07:00 -0500

    refactor: Make CWallet:::AttachChain return scan status
    Make AttachChain just responsible for syncing to the chain, moving error
    handling and chain assignment to the caller before more syncing
    functionality is moved in the next commit.
    
    Also, start using the AttachChain method in tests instead of trying
    attach in a more partial way using Chain::handleNotifications.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -329,6 +329,7 @@ private:
     // ScriptPubKeyMan::GetID. In many cases it will be the hash of an internal structure
     std::map<uint256, std::unique_ptr<ScriptPubKeyMan>> m_spk_managers;
 
+<<<<<<< HEAD
     /**
      * Catch wallet up to current chain, scanning new blocks, updating the best
      * block locator and m_last_block_processed, and registering for
@@ -336,6 +337,20 @@ private:
      */
     static bool AttachChain(const std::shared_ptr<CWallet>& wallet, interfaces::Chain& chain, bilingual_str& error, std::vector<bilingual_str>& warnings);
 
+||||||| parent of b122301d252 (refactor: Make CWallet:::AttachChain return scan status)
+    bool CreateTransactionInternal(const std::vector<CRecipient>& vecSend, CTransactionRef& tx, CAmount& nFeeRet, int& nChangePosInOut, bilingual_str& error, const CCoinControl& coin_control, FeeCalculation& fee_calc_out, bool sign) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+
+    /**
+     * Catch wallet up to current chain, scanning new blocks, updating the best
+     * block locator and m_last_block_processed, and registering for
+     * notifications about new blocks and transactions.
+     */
+    static bool AttachChain(const std::shared_ptr<CWallet>& wallet, interfaces::Chain& chain, bilingual_str& error, std::vector<bilingual_str>& warnings);
+
+=======
+    bool CreateTransactionInternal(const std::vector<CRecipient>& vecSend, CTransactionRef& tx, CAmount& nFeeRet, int& nChangePosInOut, bilingual_str& error, const CCoinControl& coin_control, FeeCalculation& fee_calc_out, bool sign) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+
+>>>>>>> b122301d252 (refactor: Make CWallet:::AttachChain return scan status)
 public:
     /**
      * Main wallet lock.
@@ -399,6 +414,16 @@ public:
 
     /** Registered interfaces::Chain::Notifications handler. */
     std::unique_ptr<interfaces::Handler> m_chain_notifications_handler;
+
+    /** Result of scanning a chain for new transactions */
+    enum class ScanStatus { SUCCESS, FAILED, MISSING_BLOCKS, SKIPPED };
+
+    /**
+     * Catch wallet up to current chain, scanning new blocks, updating the best
+     * block locator and m_last_block_processed, and registering for
+     * notifications about new blocks and transactions.
+     */
+    static ScanStatus AttachChain(std::shared_ptr<CWallet> wallet, bool scan = true);
 
     /** Interface for accessing chain state. */
     interfaces::Chain& chain() const { assert(m_chain); return *m_chain; }
