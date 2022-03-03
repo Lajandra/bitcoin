f05264f1422cce30f7381354348515449e3faf82
Ryan Ofsky <ryan@ofsky.org>
2022-03-03 14:40:18 -0500

    Disallow more unsafe string->path conversions allowed by path append operators
    Add more fs::path operator/ and operator+ overloads to prevent unsafe
    string->path conversions on Windows that would cause strings to be
    decoded according to the current Windows locale & code page instead of
    the correct string encoding.
    
    Update application code to deal with loss of implicit string->path
    conversions by calling fs::u8path or fs::PathFromString explicitly, or
    by just changing variable types from std::string to fs::path to avoid
    conversions altoghther, or make them happen earlier.
    
    In all cases, there's no change in behavior either (1) because strings
    only contained ASCII characters and would be decoded the same regardless
    of what encoding was used, or (2) because of the 1:1 mapping between
    paths and strings using the PathToString and PathFromString functions.

--- src/wallet/test/db_tests.cpp
+++ src/wallet/test/db_tests.cpp
@@ -15,22 +15,38 @@
 namespace wallet {
 BOOST_FIXTURE_TEST_SUITE(db_tests, BasicTestingSetup)
 
-static std::shared_ptr<BerkeleyEnvironment> GetWalletEnv(const fs::path& path, std::string& database_filename)
+static std::shared_ptr<BerkeleyEnvironment> GetWalletEnv(const fs::path& path, fs::path& database_filename)
 {
     fs::path data_file = BDBDataFile(path);
+<<<<<<< HEAD
     database_filename = fs::PathToString(data_file.filename());
     return GetBerkeleyEnv(data_file.parent_path(), false);
+||||||| parent of 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
+    database_filename = fs::PathToString(data_file.filename());
+    return GetBerkeleyEnv(data_file.parent_path());
+=======
+    database_filename = data_file.filename();
+    return GetBerkeleyEnv(data_file.parent_path());
+>>>>>>> 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
 }
 
 BOOST_AUTO_TEST_CASE(getwalletenv_file)
 {
+<<<<<<< HEAD
     std::string test_name = "test_name.dat";
     const fs::path datadir = m_args.GetDataDirNet();
+||||||| parent of 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
+    std::string test_name = "test_name.dat";
+    const fs::path datadir = gArgs.GetDataDirNet();
+=======
+    fs::path test_name = "test_name.dat";
+    const fs::path datadir = gArgs.GetDataDirNet();
+>>>>>>> 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
     fs::path file_path = datadir / test_name;
     std::ofstream f{file_path};
     f.close();
 
-    std::string filename;
+    fs::path filename;
     std::shared_ptr<BerkeleyEnvironment> env = GetWalletEnv(file_path, filename);
     BOOST_CHECK_EQUAL(filename, test_name);
     BOOST_CHECK_EQUAL(env->Directory(), datadir);
@@ -38,10 +54,18 @@ BOOST_AUTO_TEST_CASE(getwalletenv_file)
 
 BOOST_AUTO_TEST_CASE(getwalletenv_directory)
 {
+<<<<<<< HEAD
     std::string expected_name = "wallet.dat";
     const fs::path datadir = m_args.GetDataDirNet();
+||||||| parent of 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
+    std::string expected_name = "wallet.dat";
+    const fs::path datadir = gArgs.GetDataDirNet();
+=======
+    fs::path expected_name = "wallet.dat";
+    const fs::path datadir = gArgs.GetDataDirNet();
+>>>>>>> 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
 
-    std::string filename;
+    fs::path filename;
     std::shared_ptr<BerkeleyEnvironment> env = GetWalletEnv(datadir, filename);
     BOOST_CHECK_EQUAL(filename, expected_name);
     BOOST_CHECK_EQUAL(env->Directory(), datadir);
@@ -49,9 +73,19 @@ BOOST_AUTO_TEST_CASE(getwalletenv_directory)
 
 BOOST_AUTO_TEST_CASE(getwalletenv_g_dbenvs_multiple)
 {
+<<<<<<< HEAD
     fs::path datadir = m_args.GetDataDirNet() / "1";
     fs::path datadir_2 = m_args.GetDataDirNet() / "2";
     std::string filename;
+||||||| parent of 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
+    fs::path datadir = gArgs.GetDataDirNet() / "1";
+    fs::path datadir_2 = gArgs.GetDataDirNet() / "2";
+    std::string filename;
+=======
+    fs::path datadir = gArgs.GetDataDirNet() / "1";
+    fs::path datadir_2 = gArgs.GetDataDirNet() / "2";
+    fs::path filename;
+>>>>>>> 67ca71e5b7e (Disallow more unsafe string->path conversions allowed by path append operators)
 
     std::shared_ptr<BerkeleyEnvironment> env_1 = GetWalletEnv(datadir, filename);
     std::shared_ptr<BerkeleyEnvironment> env_2 = GetWalletEnv(datadir, filename);
@@ -65,7 +99,7 @@ BOOST_AUTO_TEST_CASE(getwalletenv_g_dbenvs_free_instance)
 {
     fs::path datadir = gArgs.GetDataDirNet() / "1";
     fs::path datadir_2 = gArgs.GetDataDirNet() / "2";
-    std::string filename;
+    fs::path filename;
 
     std::shared_ptr <BerkeleyEnvironment> env_1_a = GetWalletEnv(datadir, filename);
     std::shared_ptr <BerkeleyEnvironment> env_2_a = GetWalletEnv(datadir_2, filename);
