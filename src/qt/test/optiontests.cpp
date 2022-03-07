560431097d52b1eaacdef803b99cdd4c9d9dbd6a
Ryan Ofsky <ryan@ofsky.org>
2022-03-07 13:29:46 -0500

    qt: Avoid crash on startup if int specified in settings.json
    Fix GUI startup crash reported by Rspigler in
    https://github.com/bitcoin/bitcoin/issues/24457 that happens if
    settings.json contains an integer value for any of the configuration
    options which GUI settings can currently clash with (-dbcache, -par,
    -spendzeroconfchange, -signer, -upnp, -natpmp, -listen, -server, -proxy,
    -proxy, -onion, -onion, -lang, and -prune).
    
    Fix is a one-line change in ArgsManager::GetArg.

--- src/qt/test/optiontests.cpp
+++ src/qt/test/optiontests.cpp
@@ -23,7 +23,7 @@ void OptionTests::optionTests()
         settings.rw_settings["prune"] = 3814;
     });
     gArgs.WriteSettingsFile();
-    QVERIFY_EXCEPTION_THROWN(OptionsModel{}, std::runtime_error);
+    OptionsModel{};
     gArgs.LockSettings([&](util::Settings& settings) {
         settings.rw_settings.erase("prune");
     });
