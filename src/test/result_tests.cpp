5620bf5c36a1372ff74f9aa798a0085c2350ad78
Ryan Ofsky <ryan@ofsky.org>
2022-07-18 15:48:04 -0400

    refactor: Add util::Result class
    Add new util::Result class. The util::Result class adds two major features
    missing from BResult:
    
    - For better error handling, util::Result adds the ability to return a value on
      failure, not just a value on success. This is a key feature missing in
      BResult that makes BResult not useful for functions like LoadChainstate()
      which produce different errors that need to be handled differently
    
    - For better error reporting, util::Result adds the ability to return warning
      messages and multiple errors, not just a single error string. This provides a
      way for functions to report errors and warnings in a standard way, and
      simplifies interfaces.
    
    The util::Result class also has a few other advantages over BResult:
    
    - Smaller type size. util::Result<int> is 16 bytes, and util::Result<void> is 8
      bytes. BResult<int> is 72 bytes and BResult<void> is unimplemented.
    
    - Broader type compatibility. Supports Result<void> return values, so it is
      possible for functions that don't return values to still report errors.
      Supports Result<bilingual_str> return values to hold translated messages
      which are not errors. Supports noncopyable and nonmovable success and error
      types.
    
    - More explicit API. Drops potentially misleading BResult constructor that
      treats any bilingual string argument as an error. Adds util::Error
      constructor so it is never ambiguous when a result is being assigned an error
      or non-error value.
    
    - More standard and consistent API. util::Result supports most of the same
      operators and methods as std::optional. BResult a has less familiar interface
      with HasRes/GetObj/ReleaseObj methods. The Result/Res/Obj naming is also not
      internally consistent.
    
    - Better code organization. Puts src/util/ code in the util:: namespace so
      naming reflects code organization and it is obvious where the class is coming
      from. Drops "B" from name because it is undocumented what it stands for
      (bilingual?)
    
    - Has unit tests.

--- src/test/result_tests.cpp
+++ src/test/result_tests.cpp
@@ -6,19 +6,10 @@
 
 #include <boost/test/unit_test.hpp>
 
-inline bool operator==(const bilingual_str& a, const bilingual_str& b)
-{
-    return a.original == b.original && a.translated == b.translated;
-}
-
-inline std::ostream& operator<<(std::ostream& os, const bilingual_str& s)
-{
-    return os << "bilingual_str('" << s.original << "' , '" << s.translated << "')";
-}
-
 BOOST_AUTO_TEST_SUITE(result_tests)
 
-struct NoCopy {
+struct NoCopy
+{
     NoCopy(int n) : m_n{std::make_unique<int>(n)} {}
     std::unique_ptr<int> m_n;
 };
@@ -30,36 +21,112 @@ bool operator==(const NoCopy& a, const NoCopy& b)
 
 std::ostream& operator<<(std::ostream& os, const NoCopy& o)
 {
-    return os << "NoCopy(" << *o.m_n << ")";
+    if (o.m_n) os << "NoCopy(" << *o.m_n << ")"; else os << "NoCopy(nullptr)";
+    return os;
 }
 
-util::Result<int> IntFn(int i, bool success)
+struct NoCopyNoMove
+{
+    NoCopyNoMove(int n) : m_n{n} {}
+    NoCopyNoMove(const NoCopyNoMove&) = delete;
+    NoCopyNoMove(NoCopyNoMove&&) = delete;
+    int m_n;
+};
+
+bool operator==(const NoCopyNoMove& a, const NoCopyNoMove& b)
+{
+    return a.m_n == b.m_n;
+}
+
+std::ostream& operator<<(std::ostream& os, const NoCopyNoMove& o)
+{
+    os << "NoCopyNoMove(" << o.m_n << ")";
+    return os;
+}
+
+util::Result<void> VoidSuccessFn()
+{
+    return {};
+}
+
+util::Result<void> VoidFailFn()
+{
+    return util::Error{Untranslated("void fail.")};
+}
+
+util::Result<int> IntSuccessFn(int ret)
+{
+    return {ret};
+}
+
+util::Result<int> IntFailFn()
+{
+    return util::Error{Untranslated("int fail.")};
+}
+
+enum FnError { ERR1, ERR2 };
+
+util::Result<int, FnError> IntErrorFn(int i, bool success)
 {
     if (success) return i;
-    return util::Error{Untranslated(strprintf("int %i error.", i))};
+    return {util::Error{Untranslated(strprintf("int %i error.", i))}, i % 2 ? ERR1 : ERR2};
 }
 
-util::Result<bilingual_str> StrFn(bilingual_str s, bool success)
+util::Result<NoCopy, FnError> NoCopySuccessFn(int ret)
 {
-    if (success) return s;
-    return util::Error{strprintf(Untranslated("str %s error."), s.original)};
+    return {ret};
 }
 
-util::Result<NoCopy> NoCopyFn(int i, bool success)
+util::Result<NoCopyNoMove, FnError> EnumFailFn(FnError ret)
 {
-    if (success) return {i};
-    return util::Error{Untranslated(strprintf("nocopy %i error.", i))};
+    return {util::Error{Untranslated("status fail.")}, ret};
 }
 
-template <typename T>
-void ExpectResult(const util::Result<T>& result, bool success, const bilingual_str& str)
+util::Result<void> WarnFn()
+{
+    return {util::Warning{Untranslated("warn.")}};
+}
+
+util::Result<int> MultiWarnFn(int ret)
+{
+    util::Result<void> result;
+    for (int i = 0; i < ret; ++i) {
+        result.AddWarning(strprintf(Untranslated("warn %i."), i));
+    }
+    return {std::move(result), ret};
+}
+
+util::Result<void, int> ChainedFailFn(FnError arg, int ret)
+{
+    return {util::Error{Untranslated("chained fail.")}, EnumFailFn(arg), WarnFn(), ret};
+}
+
+util::Result<NoCopyNoMove> NoCopyNoMoveSuccessFn(int ret)
+{
+    return {ret};
+}
+
+util::Result<int, FnError> AccumulateFn(bool success)
+{
+    util::Result<int, FnError> result;
+    util::Result<int> x = result << MultiWarnFn(1);
+    BOOST_REQUIRE(x);
+    util::Result<int> y = result << MultiWarnFn(2);
+    BOOST_REQUIRE(y);
+    result = IntErrorFn(*x + *y, success);
+    return result;
+}
+
+template<typename T, typename F>
+void ExpectResult(const util::Result<T, F>& result, bool success, const bilingual_str& str)
 {
     BOOST_CHECK_EQUAL(bool(result), success);
-    BOOST_CHECK_EQUAL(util::ErrorString(result), str);
+    BOOST_CHECK_EQUAL(ErrorString(result).original, str.original);
+    BOOST_CHECK_EQUAL(ErrorString(result).translated, str.translated);
 }
 
-template <typename T, typename... Args>
-void ExpectSuccess(const util::Result<T>& result, const bilingual_str& str, Args&&... args)
+template<typename T, typename F, typename... Args>
+void ExpectSuccessValue(const util::Result<T, F>& result, const bilingual_str& str, Args&&... args)
 {
     ExpectResult(result, true, str);
     BOOST_CHECK_EQUAL(result.has_value(), true);
@@ -67,30 +134,37 @@ void ExpectSuccess(const util::Result<T>& result, const bilingual_str& str, Args
     BOOST_CHECK_EQUAL(&result.value(), &*result);
 }
 
-template <typename T, typename... Args>
-void ExpectFail(const util::Result<T>& result, const bilingual_str& str)
+template<typename T, typename F, typename... Args>
+void ExpectFailValue(const util::Result<T, F>& result, bilingual_str str, Args&&... args)
 {
     ExpectResult(result, false, str);
+    BOOST_CHECK_EQUAL(result.GetFailure(), F{std::forward<Args>(args)...});
 }
 
 BOOST_AUTO_TEST_CASE(check_returned)
 {
-    ExpectSuccess(IntFn(5, true), {}, 5);
-    ExpectFail(IntFn(5, false), Untranslated("int 5 error."));
-    ExpectSuccess(NoCopyFn(5, true), {}, 5);
-    ExpectFail(NoCopyFn(5, false), Untranslated("nocopy 5 error."));
-    ExpectSuccess(StrFn(Untranslated("S"), true), {}, Untranslated("S"));
-    ExpectFail(StrFn(Untranslated("S"), false), Untranslated("str S error."));
+    ExpectResult(VoidSuccessFn(), true, {});
+    ExpectResult(VoidFailFn(), false, Untranslated("void fail."));
+    ExpectSuccessValue(IntSuccessFn(5), {}, 5);
+    ExpectResult(IntFailFn(), false, Untranslated("int fail."));
+    ExpectSuccessValue(NoCopySuccessFn(5), {}, 5);
+    ExpectFailValue(EnumFailFn(ERR2), Untranslated("status fail."), ERR2);
+    ExpectFailValue(ChainedFailFn(ERR1, 5), Untranslated("chained fail. status fail. warn."), 5);
+    ExpectSuccessValue(NoCopyNoMoveSuccessFn(5), {}, 5);
+    ExpectSuccessValue(MultiWarnFn(3), Untranslated("warn 0. warn 1. warn 2."), 3);
+    ExpectSuccessValue(AccumulateFn(true), Untranslated("warn 0. warn 0. warn 1."), 3);
+    ExpectFailValue(AccumulateFn(false), Untranslated("int 3 error. warn 0. warn 0. warn 1."), ERR1);
 }
 
-BOOST_AUTO_TEST_CASE(check_value_or)
+BOOST_AUTO_TEST_CASE(check_dereference_operators)
 {
-    BOOST_CHECK_EQUAL(IntFn(10, true).value_or(20), 10);
-    BOOST_CHECK_EQUAL(IntFn(10, false).value_or(20), 20);
-    BOOST_CHECK_EQUAL(NoCopyFn(10, true).value_or(20), 10);
-    BOOST_CHECK_EQUAL(NoCopyFn(10, false).value_or(20), 20);
-    BOOST_CHECK_EQUAL(StrFn(Untranslated("A"), true).value_or(Untranslated("B")), Untranslated("A"));
-    BOOST_CHECK_EQUAL(StrFn(Untranslated("A"), false).value_or(Untranslated("B")), Untranslated("B"));
+    util::Result<std::pair<int, std::string>> mutable_result;
+    const auto& const_result{mutable_result};
+    mutable_result.value() = {1, "23"};
+    BOOST_CHECK_EQUAL(mutable_result->first, 1);
+    BOOST_CHECK_EQUAL(const_result->second, "23");
+    (*mutable_result).first = 5;
+    BOOST_CHECK_EQUAL((*const_result).first, 5);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
