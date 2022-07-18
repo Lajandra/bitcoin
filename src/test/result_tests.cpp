779966d0c19d9a90ea8a267e9433b8cd08e538af
Ryan Ofsky <ryan@ofsky.org>
2022-07-18 15:48:04 -0400

    BResult improvements
    This is a draft commit trying to make some improvements to the `BResult` class to make it more usable. Adds a `util::Result` class with the following improvements (and implements `BResult` in terms of it so existing code doesn't have to change):
    
    - Supports returning error strings and error information at the same time. This functionality is needed by the `LoadChainstate` function in #25308 or any function that can fail in multiple ways which need to be handled differently. And it means Result class is compatible with rust-style error handling and pattern matching, unlike BResult.
    
    - Makes Result class provide same operators and methods as std::optional, so it doesn't require calling less familiar `HasRes`/`GetObj`/`ReleaseObj` methods, and so error reporting functionality can be easily added or dropped from existing code by switching between util::Result and std::optional.
    
    - Supports `Result<void>` return values, so it is possible to return error reporting information from functions in a uniform way even if they don't have other return values.
    
    - Supports `Result<bilingual_str>` return values. Drops ambiguous and potentially misleading `BResult` constructor that treats any bilingual string as an error, and any other type as a non-error. Adds `util::Error` constructor so errors have to be explicitly constructed and not any `bilingual_str` will be turned into one.
    
    - Puts `src/util/` code in the `util::` namespace so naming reflects code organization and it is obvious where the class is coming from. Drops "B" from name because it is undocumented what it stands for (bilingual?)
    
    - Adds unit tests.

---
+++ src/test/result_tests.cpp
@@ -0,0 +1,157 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/result.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(result_tests)
+
+struct NoCopy
+{
+    NoCopy(int n) : m_n{std::make_unique<int>(n)} {}
+    std::unique_ptr<int> m_n;
+};
+
+bool operator==(const NoCopy& a, const NoCopy& b)
+{
+    return *a.m_n == *b.m_n;
+}
+
+std::ostream& operator<<(std::ostream& os, const NoCopy& o)
+{
+    if (o.m_n) os << "NoCopy(" << *o.m_n << ")"; else os << "NoCopy(nullptr)";
+    return os;
+}
+
+struct NoCopyNoMove
+{
+    NoCopyNoMove(int n) : m_n{n} {}
+    NoCopyNoMove(const NoCopyNoMove&) = delete;
+    NoCopyNoMove(NoCopyNoMove&&) = delete;
+    int m_n;
+};
+
+bool operator==(const NoCopyNoMove& a, const NoCopyNoMove& b)
+{
+    return a.m_n == b.m_n;
+}
+
+std::ostream& operator<<(std::ostream& os, const NoCopyNoMove& o)
+{
+    os << "NoCopyNoMove(" << o.m_n << ")";
+    return os;
+}
+
+util::Result<void> VoidSuccessFn()
+{
+    return {};
+}
+
+util::Result<void> VoidFailFn()
+{
+    return {util::Error{}, Untranslated("void fail")};
+}
+
+util::Result<int> IntSuccessFn(int ret)
+{
+    return {ret};
+}
+
+util::Result<int> IntFailFn()
+{
+    return {util::Error{}, Untranslated("int fail")};
+}
+
+enum FnError { ERR1, ERR2 };
+
+util::Result<NoCopy, FnError> NoCopySuccessFn(int ret)
+{
+    return {ret};
+}
+
+util::Result<NoCopyNoMove, FnError> EnumFailFn(FnError ret)
+{
+    return {util::Error{}, Untranslated("status fail"), ret};
+}
+
+util::Result<void> WarnFn()
+{
+    return {util::Warning{}, Untranslated("warn")};
+}
+
+util::Result<int> MultiWarnFn(int ret)
+{
+    util::Result<void> result;
+    for (int i = 0; i < ret; ++i) {
+        result.AddWarning(strprintf(Untranslated("warn %i"), i));
+    }
+    return {util::Chain{}, std::move(result), ret};
+}
+
+util::Result<void, int> ChainedFailFn(FnError arg, int ret)
+{
+    return {util::Chain{}, EnumFailFn(arg), util::Chain{}, EnumFailFn(arg), util::Error{}, Untranslated("chained fail"),  ret};
+}
+
+
+util::Result<NoCopyNoMove> NoCopyNoMoveSuccessFn(int ret)
+{
+    return {ret};
+}
+
+template<typename T, typename F>
+void ExpectSuccess(const util::Result<T, F>& result)
+{
+    BOOST_CHECK(result);
+}
+
+template<typename T, typename F>
+void ExpectFail(const util::Result<T, F>& result, bilingual_str str)
+{
+    BOOST_CHECK(!result);
+    BOOST_CHECK_EQUAL(ErrorString(result).original, str.original);
+    BOOST_CHECK_EQUAL(ErrorString(result).translated, str.translated);
+}
+
+template<typename T, typename F, typename... Args>
+void ExpectSuccessValue(const util::Result<T, F>& result, Args&&... args)
+{
+    ExpectSuccess(result);
+    BOOST_CHECK_EQUAL(result.has_value(), true);
+    BOOST_CHECK_EQUAL(result.value(), T{std::forward<Args>(args)...});
+    BOOST_CHECK_EQUAL(&result.value(), &*result);
+}
+
+template<typename T, typename F, typename... Args>
+void ExpectFailValue(const util::Result<T, F>& result, bilingual_str str, Args&&... args)
+{
+    ExpectFail(result, str);
+    BOOST_CHECK_EQUAL(result.GetFailure(), F{std::forward<Args>(args)...});
+}
+
+BOOST_AUTO_TEST_CASE(check_returned)
+{
+    ExpectSuccess(VoidSuccessFn());
+    ExpectFail(VoidFailFn(), Untranslated("void fail"));
+    ExpectSuccessValue(IntSuccessFn(5), 5);
+    ExpectFail(IntFailFn(), Untranslated("int fail"));
+    ExpectSuccessValue(NoCopySuccessFn(5), 5);
+    ExpectFailValue(EnumFailFn(ERR2), Untranslated("status fail"), ERR2);
+    ExpectFailValue(ChainedFailFn(ERR1, 5), Untranslated("status fail, chained fail"), 5);
+    ExpectSuccessValue(NoCopyNoMoveSuccessFn(5), 5);
+}
+
+BOOST_AUTO_TEST_CASE(check_dereference_operators)
+{
+    util::Result<std::pair<int, std::string>> mutable_result;
+    const auto& const_result{mutable_result};
+    mutable_result.value() = {1, "23"};
+    BOOST_CHECK_EQUAL(mutable_result->first, 1);
+    BOOST_CHECK_EQUAL(const_result->second, "23");
+    (*mutable_result).first = 5;
+    BOOST_CHECK_EQUAL((*const_result).first, 5);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
