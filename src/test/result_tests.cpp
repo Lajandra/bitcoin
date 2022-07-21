e71b858bc0f30784191725d71a77bab4d280a6f9
Ryan Ofsky <ryan@ofsky.org>
2022-07-21 08:30:39 -0400

    refactor: Replace BResult with util::Result
    Rename BResult class to util::Result and update the class interface to be more
    compatible with std::optional and with a full-featured result class implemented
    in https://github.com/bitcoin/bitcoin/pull/25665. Motivation for this change is
    to update existing BResult usages now so they don't have to change later when
    more features are added to the Result class.

---
+++ src/test/result_tests.cpp
@@ -0,0 +1,71 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/result.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(result_tests)
+
+struct NoCopy
+{
+    NoCopy(int n) : m_n{std::make_unique<int>(n)} {}
+    std::unique_ptr<int> m_n;
+};
+
+bool operator==(const NoCopy& a, const NoCopy& b)
+{
+    return *a.m_n == *b.m_n;
+}
+
+std::ostream& operator<<(std::ostream& os, const NoCopy& o)
+{
+    if (o.m_n) os << "NoCopy(" << *o.m_n << ")"; else os << "NoCopy(nullptr)";
+    return os;
+}
+
+util::Result<int> IntFn(int i, bool success)
+{
+    if (success) return i;
+    return {util::Error{Untranslated(strprintf("int %i error.", i))}};
+}
+
+util::Result<NoCopy> NoCopyFn(int i, bool success)
+{
+    if (success) return {i};
+    return {util::Error{Untranslated(strprintf("nocopy %i error.", i))}};
+}
+
+template<typename T>
+void ExpectResult(const util::Result<T>& result, bool success, const bilingual_str& str)
+{
+    BOOST_CHECK_EQUAL(bool(result), success);
+    BOOST_CHECK_EQUAL(ErrorString(result).original, str.original);
+    BOOST_CHECK_EQUAL(ErrorString(result).translated, str.translated);
+}
+
+template<typename T, typename... Args>
+void ExpectSuccess(const util::Result<T>& result, const bilingual_str& str, Args&&... args)
+{
+    ExpectResult(result, true, str);
+    BOOST_CHECK_EQUAL(result.has_value(), true);
+    BOOST_CHECK_EQUAL(result.value(), T{std::forward<Args>(args)...});
+    BOOST_CHECK_EQUAL(&result.value(), &*result);
+}
+
+template<typename T, typename... Args>
+void ExpectFail(const util::Result<T>& result, const bilingual_str& str)
+{
+    ExpectResult(result, false, str);
+}
+
+BOOST_AUTO_TEST_CASE(check_returned)
+{
+    ExpectSuccess(IntFn(5, true), {}, 5);
+    ExpectFail(IntFn(5, false), Untranslated("int 5 error."));
+    ExpectSuccess(NoCopyFn(5, true), {}, 5);
+    ExpectFail(NoCopyFn(5, false), Untranslated("nocopy 5 error."));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
--- src/util/result.h
+++ src/util/result.h
@@ -9,41 +9,60 @@
 
 #include <variant>
 
-/*
- * 'BResult' is a generic class useful for wrapping a return object
- * (in case of success) or propagating the error cause.
-*/
+namespace util {
+
+struct Error { bilingual_str message; };
+
+//! The util::Result class provides a standard way for functions to return error
+//! and in addition to optional result values.
+//!
+//! It is intended for high-level functions that need to report error strings to
+//! end users. Lower-level functions that don't need this error-reporting and
+//! only need error-handling should avoid util::Result and instead use standard
+//! classes like std::optional, std::variant, and std::tuple, or custom structs
+//! and enum types to return function results.
+//!
+//! Usage examples can be found in \example ../test/result_tests.cpp, but in
+//! general code returning `util::Result<T>` values is very similar to code
+//! returning `std::optional<T>` values. Existing functions returning
+//! `std::optional<T>` can be updated to return `util::Result<T>` and return
+//! error strings usually just replacing `return std::nullopt;` with `return
+//! util::Error{error_string};`.
 template<class T>
-class BResult {
+class Result {
 private:
     std::variant<bilingual_str, T> m_variant;
 
+    template <typename FT>
+    friend class Result;
+    template <typename FT>
+    friend bilingual_str ErrorString(const Result<FT>& result);
+
 public:
-    BResult() : m_variant{Untranslated("")} {}
-    BResult(T obj) : m_variant{std::move(obj)} {}
-    BResult(bilingual_str error) : m_variant{std::move(error)} {}
+    Result(T obj) : m_variant{std::move(obj)} {}
+    Result(Error error) : m_variant{std::move(error.message)} {}
+    template <typename OT>
+    Result(Error error, Result<OT>&& other) : m_variant{std::move(std::get<0>(other.m_variant))} {}
 
-    /* Whether the function succeeded or not */
-    bool HasRes() const { return std::holds_alternative<T>(m_variant); }
-
-    /* In case of success, the result object */
-    const T& GetObj() const {
-        assert(HasRes());
-        return std::get<T>(m_variant);
-    }
-    T ReleaseObj()
-    {
-        assert(HasRes());
-        return std::move(std::get<T>(m_variant));
-    }
-
-    /* In case of failure, the error cause */
-    const bilingual_str& GetError() const {
-        assert(!HasRes());
-        return std::get<bilingual_str>(m_variant);
-    }
-
-    explicit operator bool() const { return HasRes(); }
+    //! std::optional methods, so functions returning optional<T> can change to
+    //! return Result<T> with minimal changes to existing code, and vice versa.
+    bool has_value() const { return m_variant.index() == 1; }
+    const T& value() const { assert(*this); return std::get<1>(m_variant); }
+    T& value() { assert(*this); return std::get<1>(m_variant); }
+    template <typename U>
+    T value_or(const U& default_value) const { return has_value() ? value() : default_value; }
+    operator bool() const { return has_value(); }
+    const T* operator->() const { return &value(); }
+    const T& operator*() const { return value(); }
+    T* operator->() { return &value(); }
+    T& operator*() { return value(); }
 };
+
+template <typename T>
+bilingual_str ErrorString(const Result<T>& result)
+{
+    return result ? bilingual_str{} : std::get<0>(result.m_variant);
+}
+} // namespace util
 
 #endif // BITCOIN_UTIL_RESULT_H
