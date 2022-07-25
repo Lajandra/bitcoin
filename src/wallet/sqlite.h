2c675f03f017125390f3b578ee6169870deae985
Ryan Ofsky <ryan@ofsky.org>
2022-07-25 17:40:49 -0400

    refactor: Use util::Result class for wallet loading
    Wallet loading functions up and down the stack have lots of error and warning
    parameters, and return error information in different ways. This PR makes them
    uniformly return util::Result, without changing behavior.

--- src/wallet/sqlite.h
+++ src/wallet/sqlite.h
@@ -73,7 +73,7 @@ public:
 
     ~SQLiteDatabase();
 
-    bool Verify(bilingual_str& error);
+    util::Result<void> Verify();
 
     /** Open the database if it is not already opened */
     void Open() override;
@@ -116,7 +116,7 @@ public:
     bool m_use_unsafe_sync;
 };
 
-std::unique_ptr<SQLiteDatabase> MakeSQLiteDatabase(const fs::path& path, const DatabaseOptions& options, DatabaseStatus& status, bilingual_str& error);
+util::Result<std::unique_ptr<SQLiteDatabase>, DatabaseError> MakeSQLiteDatabase(const fs::path& path, const DatabaseOptions& options);
 
 std::string SQLiteDatabaseVersion();
 } // namespace wallet
